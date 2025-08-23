9a873de30b6839cfb3442c8449954d8af3e1d3c5
Makr91 <mark.gilbert@m4kr.net>
2025-08-23 15:47:30 -0500

    fix: Zones Page v2

--- web/src/components/Zones.jsx
+++ web/src/components/Zones.jsx
@@ -1352,8 +1352,8 @@ const Zones = () => {
         <title>Zones - Zoneweaver</title>
         <link rel='canonical' href={window.location.origin} />
       </Helmet>
-      <div className='container is-fluid m-2'>
-        <div className='box p-0'>
+      <div className='container is-fluid p-0'>
+        <div className='box p-0 is-radiusless'>
           <div className='titlebar box active level is-mobile mb-0 p-3'>
             <div className='level-left'>
               <strong>Zone Management</strong>
