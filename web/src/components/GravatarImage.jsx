ad9d1f7449c5207879aca9d9657683c885092ea8
Makr91 <mark.gilbert@m4kr.net>
2025-08-29 21:56:12 -0500

    feat: scss reduction v22 - gravtar image

--- web/src/components/GravatarImage.jsx
+++ web/src/components/GravatarImage.jsx
@@ -5,10 +5,20 @@ export default function GravatarImage() {
     const { user } = useAuth();
 
     if (!user || !user.gravatar) {
-        return <span className="skeleton__avatar is-32x32 is-rounded" />;
+        return (
+            <figure className="image is-32x32">
+                <span className="skeleton__avatar is-rounded" />
+            </figure>
+        );
     }
  
     return (
-        <img src={ user.gravatar.avatar_url + '?size=64' } className="is-32x32" style={{ borderRadius: '50%' }} />
+        <figure className="image is-32x32">
+            <img 
+                src={user.gravatar.avatar_url + '?size=32'} 
+                className="is-rounded" 
+                alt="User avatar"
+            />
+        </figure>
     )
 }
